11d87aad23ab190644a4e63b0cf9abe005233475
dParadiz <dParadiz@users.noreply.github.com>
2026-04-18 22:33:57 +0000

    Deploy from ${GITHUB_SHA} 6757ff050b81b67f744c489d2cf5fbd8869cebae

--- assets/__federation_expose_GameInfo-CEry4XI5.js
+++
@@ -1,1 +0,0 @@
-import{_ as f}from"./GameInfo.vue_vue_type_script_setup_true_lang-6XiG1ZIm.js";export{f as default};
---
+++ assets/__federation_expose_GameInfo-DgDCVmzd.js
@@ -0,0 +1,1 @@
+import{_ as f}from"./GameInfo.vue_vue_type_script_setup_true_lang-BRISFOGf.js";export{f as default};
